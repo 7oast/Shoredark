5c8f3254d663e8b86cc55112ad9c945a56ea82ce
Titancube <kbin0717@gmail.com>
2019-01-16 09:51:03 -0500

    uptime function added

--- shoredark.js
+++ shoredark.js
@@ -6,20 +6,19 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-let totalSeconds = (client.uptime / 1000);
-let days = Math.floor(totalSeconds / 86400);
-let hours = Math.floor(totalSeconds / 3600);
-totalSeconds %= 3600;
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60;
-let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
-
 client.on('message', msg => {
   if (msg.content === '$hi') {
     msg.reply('Ayyo');
   }
   if (msg.content === '$uptime') {
-    msg.channel.send(`The bot is up for ${uptime}.`);
+    let totalSeconds = (client.uptime / 1000);
+    let days = Math.floor(totalSeconds / 86400);
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+    msg.channel.send(`The bot is up for `+uptime);
   }
 });
 

991a276ec824ba6e1eb6e991ed79ea1344568e52
Titancube <kbin0717@gmail.com>
2019-01-16 09:07:53 -0500

    modified:   shoredark.js

--- shoredark.js
+++ shoredark.js
@@ -1,13 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require('dotenv').config();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
+let totalSeconds = (client.uptime / 1000);
+let days = Math.floor(totalSeconds / 86400);
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  if (msg.content === '$hi') {
+    msg.reply('Ayyo');
+  }
+  if (msg.content === '$uptime') {
+    msg.channel.send(`The bot is up for ${uptime}.`);
   }
 });
 

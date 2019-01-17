13bc3bec120d1c4a9976d5126e1764aca205040a
Titancube <kbin0717@gmail.com>
2019-01-17 06:49:41 -0500

    modified:   shoredark.js

--- shoredark.js
+++ shoredark.js
@@ -46,7 +46,7 @@ client.on('message', msg => {
         value: "\u200B"
       },{
         name: "명령어",
-        value: "$help: DM 채널을 통해 도움말을 띄웁니다. \n $uptime: 봇이 실행된지 얼마나 지났는지 보여줍니다. \n $장준: 아이고 \n $radenika: 이제 똥겜 안한대요 \n $stepic: 머저리겜 그만해"
+        value: "$help: DM 채널을 통해 도움말을 띄웁니다. \n $uptime: 봇이 실행된지 얼마나 지났는지 보여줍니다. \n $장준: 아이고 \n $radenika: 이제 똥겜 안한대요 \n $stepic: 머저리겜 그만해 \n $전역: Titancube의 전역이... 얼마나 남았는지... 보여줍니다..."
       }
     ],
     timestamp: new Date(),
@@ -56,6 +56,30 @@ client.on('message', msg => {
     }
   }
 });
+  }
+  if (msg.content === '$전역') {
+    let retire = new Date("Apr 27, 2019 08:00:00").getTime();
+    // Get todays date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = retire - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    let retirecount = days + "일 " + hours + "시간 " + minutes + "분 " + seconds + "초";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      msg.channel.send(`전역이다!`);
+    } else {
+      msg.channel.send(`Titancube 전역까지 ${retirecount} 남았습니다...`);
+    }
   }
 });
 

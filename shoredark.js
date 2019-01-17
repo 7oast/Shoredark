ca2e47bcda1daca257b313d4f85b54f73c519282
Titancube <kbin0717@gmail.com>
2019-01-17 06:26:34 -0500

    modified:   shoredark.js

--- shoredark.js
+++ shoredark.js
@@ -29,6 +29,34 @@ client.on('message', msg => {
   if (msg.content === '$stepic') {
     msg.channel.send('당신의 인생 머저리겜으로 대체되었다');
   }
+  if (msg.content === '$help') {
+    msg.reply('DM 채널을 확인하세요!');
+    msg.author.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "v0.1",
+    fields: [{
+        name: "아직은 테스트 중입니다!",
+        value: "별 다른 기능이 없습니다. 아직은."
+      },{
+        name: "\u200B",
+        value: "\u200B"
+      },{
+        name: "명령어",
+        value: "$help: DM 채널을 통해 도움말을 띄웁니다. \n $uptime: 봇이 실행된지 얼마나 지났는지 보여줍니다. \n $장준: 아이고 \n $radenika: 이제 똥겜 안한대요 \n $stepic: 머저리겜 그만해"
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Titancube"
+    }
+  }
+});
+  }
 });
 
 client.login(process.env.BOT_TOKEN);

457a294417811ff47766c0ea512b036b0b87a2ad
titancube <kbin0717@gmail.com>
2019-02-28 08:09:21 -0500

    Remove unused variables

--- commands/role.js
+++ commands/role.js
@@ -1,13 +1,7 @@
-const { prefix } = require('../config.json');
 module.exports = {
     name: "role",
     description: "원하는 역할을 얻을 수 있습니다.",
-    execute(msg, args) {
-      const mscn = "549948561089888256",
-        spkr = "549949028939464714",
-        undg = "549948611639771146"
-      const { commands } = msg.client
-      
+    execute(msg, args) {      
       if (!args.length) return msg.reply('no argument')
       
       const role = args[0].charAt(0).toUpperCase() + args[0].slice(1).toLowerCase()

01038ef3bfa388a68304c7c41d3bde265744675f
titancube <kbin0717@gmail.com>
2019-03-04 19:28:45 -0500

    Fix roles

--- commands/role.js
+++ commands/role.js
@@ -7,10 +7,6 @@ module.exports = {
       const role = args[0].charAt(0).toUpperCase() + args[0].slice(1).toLowerCase()
       const allowedRole = msg.guild.roles.find(nm => nm.name === role)
       if (!allowedRole) return
-      if (allowedRole.name === '관리자' || allowedRole.name === '커뮤니티 큐레이터' || allowedRole.name === 'NAS') {
-        msg.reply('접근 권한이 없습니다.')
-        return
-      } 
       if (msg.member.roles.has(allowedRole.id)) {
         msg.member.removeRole(allowedRole.id).catch(console.error)
         msg.reply(`${allowedRole.name} 권한을 제거했습니다.`)

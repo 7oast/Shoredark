7fa68e2db7412cab7b4896a1c720a9c81e6f0c91
titancube <kbin0717@gmail.com>
2019-04-28 00:35:57 +0900

    Beautify

--- commands/retire.js
+++ commands/retire.js
@@ -4,25 +4,25 @@ module.exports = {
   execute(msg, args) {
     // Today
     const now = new Date().getTime()
-//     let soldiers = [];
+    //     let soldiers = [];
 
-//     // Make a new soldier
-//     function soldier(name, retire) {
-//       this.name = name;
-//       this.retire = new Date(retire).getTime();
-//       this.distance = retire - now;
-//       // this.remain = calculateRetire(this.distance);
-//       soldiers.push({name: this.name, retire: this.retire, distance: this.distance});
-//     }
+    //     // Make a new soldier
+    //     function soldier(name, retire) {
+    //       this.name = name;
+    //       this.retire = new Date(retire).getTime();
+    //       this.distance = retire - now;
+    //       // this.remain = calculateRetire(this.distance);
+    //       soldiers.push({name: this.name, retire: this.retire, distance: this.distance});
+    //     }
 
-//     const Aiobahn = new soldier("Aiobahn", "Oct 13, 2019 08:00:00");
-//     const Aresynth = new soldier("Aresynth", "Oct 14, 2019 08:00:00");
+    //     const Aiobahn = new soldier("Aiobahn", "Oct 13, 2019 08:00:00");
+    //     const Aresynth = new soldier("Aresynth", "Oct 14, 2019 08:00:00");
 
     let aioRetire = new Date("Oct 13, 2019 08:00:00 +1000").getTime()
     let aresynthRetire = new Date("Oct 14, 2019 08:00:00 +1000").getTime()
     let toastRetire = new Date("Apr 02, 2021 08:00:00 +1000").getTime()
     // Get todays date and time
-    
+
 
     // Find the tcDistance between now and the count down date
     let aioDistance = aioRetire - now
@@ -39,10 +39,10 @@ module.exports = {
     }
 
     // MSG!
-//     for (let soldier in soldiers) {
-//       msg.channel.send(`${soldier.name}의 전역까지 ${calculateRetire(soldier.distance)} 남았습니다`)
-//     }
-    
+    //     for (let soldier in soldiers) {
+    //       msg.channel.send(`${soldier.name}의 전역까지 ${calculateRetire(soldier.distance)} 남았습니다`)
+    //     }
+
     msg.channel.send(`
 Aiobahn 전역까지 ${calculateRetire(aioDistance)} 남았습니다.
 Aresynth 전역까지 ${calculateRetire(aresynthDistance)} 남았습니다.

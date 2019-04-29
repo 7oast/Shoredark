e95d66b2689ab075a6f7a12b62d4b83f85f6a0f6
titancube <kbin0717@gmail.com>
2019-04-29 21:16:36 +0900

    Change roles

--- commands/role.js
+++ commands/role.js
@@ -4,9 +4,28 @@ module.exports = {
   execute(msg, args) {
     if (!args.length) return msg.reply('no argument')
 
+    // Fetch the argument and format
     const role = args[0].charAt(0).toUpperCase() + args[0].slice(1).toLowerCase()
     const allowedRole = msg.guild.roles.find(nm => nm.name === role)
+
+    // Calcle the procedure if the role is not existing nor allowed
     if (!allowedRole) return
+    
+    // Check availability
+    function checkPermission(rolename) {
+      switch (rolename) {
+        case "Admin":
+        case "Nas":
+        case "Pirate":
+        return false;
+        default: return true;
+      }
+    }
+
+    // Check if the role is existing and achievable by normal members of server.
+    if (!checkPermission(rolename)) return;
+
+    // Gives role if the person doesn't have it and takes it back when the one has.
     if (msg.member.roles.has(allowedRole.id)) {
       msg.member.removeRole(allowedRole.id).catch(console.error)
       msg.reply(`${allowedRole.name} 권한을 제거했습니다.`)
--- commands/rolemessage.js
+++ commands/rolemessage.js
@@ -5,12 +5,15 @@ module.exports = {
     let initialMessage = `Shorelight에서는 역할에 따라 볼 수 있는 채널 카테고리를 다르게 하고 있습니다.
 \`\`$role 역할\`\` 커맨드로 원하는 채널을 얻으세요. 만약 역할을 빼고 싶다면 다시 입력하여 제거할 수 있습니다.
 \`\`\`
-역할과 이에 대응하는 카테고리
-Creator : Creator
-Speaker: Topic-Based
-Underground : 심연
-Gamer : Gamer
+얻을 수 있는 역할
+Admin       : Admin을 포함한 모든 카테고리와 채널
+Gamer       : Gamer
+Tech        : Tech
+Student     : Edu
+Inner       : INNER
 \`\`\`
+
+\`\`* Pirate와 NAS는 비공개 초대에 의해서만 획득 가능합니다.\`\`
     `
     if (msg.author.id != '116455101811392512') return;
 
